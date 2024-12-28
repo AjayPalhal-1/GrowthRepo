190823c8dc02e790f333bd52c628d05e1685524d
Ajay Palhal <ajaypalhal63@gmail.com>
2024-12-28 17:22:52 +0530

    Map Problem

--- DSA/STL/Map/prblm1.cpp
+++ DSA/STL/Map/prblm1.cpp
@@ -23,33 +23,57 @@
 // 2 -> 2  
 // 3 -> 3
 
-
-
-#include<map>
-#include<iostream>
+#include <map>
+#include <iostream>
+#include <vector>
 using namespace std;
-
 
 int main()
 {
-    map<int , int > mymap = {{1,1},{2,2},{3,2},{4,3},{5,3}};
-    int count =1;
-    int  i =  mymap.begin()->second;
-    for (i ; i < mymap.size(); i++)
-    {
-        for (int j = i+1 ; j < mymap.size(); j++)
-        {
-          if(mymap.at( i) == mymap.at(j))
-          {
-             count++;
-             break;
+    // Input Array
+    vector<int> arr = {1, 2, 2, 3, 3, 3};
+    
+    // Declare map to store frequency
+    map<int, int> freq_map;
 
-          }
-        }  
-            cout<<mymap.at(i) <<"  Present " <<count << " :  Times \n";
-     
-        
+    for (int num : arr) {
+        freq_map[num]++;  // only unique valus storing and if no repete then vaules : updated to find occurance of no.
     }
 
-    
+    cout << "Element -> Frequency" << endl;
+    for (const auto& pair : freq_map) {
+        cout << pair.first << " -> " << pair.second << endl;
+    }
+
+    return 0;
 }
+
+
+// #include<map>
+// #include<iostream>
+// using namespace std;
+
+
+// int main()
+// {
+//     map<int , int > mymap = {{1,1},{2,2},{3,2},{4,3},{5,3}};
+//     int count =1;
+//     int  i =  mymap.begin()->second;
+//     for (i ; i < mymap.size(); i++)
+//     {
+//         for (int j = i+1 ; j < mymap.size(); j++)
+//         {
+//           if(mymap.at( i) == mymap.at(j))
+//           {
+//              count++;
+//              break;
+
+//           }
+//         }  
+//             cout<<mymap.at(i) <<"  Present " <<count << " :  Times \n";
+     
+        
+//     }
+
+    
+// }
